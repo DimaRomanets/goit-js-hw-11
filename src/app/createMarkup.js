4726756df05e945dbfa8d505b28d2c2fb99e0db5
DimaRomanets <130175375+DimaRomanets@users.noreply.github.com>
2023-09-17 22:11:20 +0200

    two

--- src/app/createMarkup.js
+++ src/app/createMarkup.js
@@ -14,8 +14,8 @@ export function createMarkup(photos) {
       }) => {
         return /*html*/ `
             
-            <div class="photo-card card-link">
-            <a href='${largeImageURL}' class="js-card-link">
+            <div class="photo-card card-link js-card-link">
+            <a href='${largeImageURL}' >
               <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
               </a>
               <div class="info">
